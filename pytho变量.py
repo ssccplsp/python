19f963ebcf0a6cfcbbeb1c45d494f853c94c8ebf
ssccp <ssccp@sina.cn>
2022-02-19 10:47:55 +0800

    Initial commit

---
+++ pytho变量.py
@@ -0,0 +1,41 @@
+'''
+1 什么是变量
+变量是指各种类型的值的名字
+变量的命名规则：
+变量由字母数字下划线组成，开头不能写数字，变量名最好见名知意
+
+'''
+'''
+Hello='hello word'
+print(Hello)
+'''
+'''
+Hello 是创建的变量，=是赋值语， 和hello word 是变量值
+'''
+'''
+同一变量可以多次赋值
+
+'''
+'''
+a=666
+print(a)
+a='hello'
+print(a)
+'''
+
+print(type('hello'))
+print(type(123))
+print(type('123'))
+
+'''
+还可以照一下方法定义变量：
+'''
+'''
+a='xianchao'
+b=a
+a='hello word'
+print(b)
+print(a)
+'''
+
+
---
+++ test.py
@@ -0,0 +1,68 @@
+'''
+python3中有6种常见的数据类型
+一：number（数字）
+二：string（字符串）
+三：list（列表）
+四：tuple（元组）
+五：sets（集合）
+六：dictionary（字典）
+'''
+
+'''
+不可变的数据类型有三种：number  tuple  string 
+可变的数据类型有三种：list  sets  dictionary
+
+'''
+
+'''
+一：number(数字)
+number有四种类型：
+int（整型） float（浮点型） complex（负数） bool（布尔）
+1  int
+    int常被称为整型或者整数  整数指正负整数不带小数点整数表示方法有四种：
+    十进制     二进制     八进制     十六进制
+    
+'''
+'''
+print("66")
+print(0b110110)
+print(0o66)
+print(0x66)
+'''
+'''
+print(bin(66))
+print(oct(66))
+print(hex(66))
+'''
+
+'''
+创造一个int类型的数
+'''
+'''
+print(12)
+print(int(12.1))
+'''
+
+'''
+float浮点型，由整数和小数部分组成
+11.2323
+16.1
+21.345
+'''
+'''
+print(12.3)
+print(float(12))
+print(float('12'))
+print(type(22))
+print(type(12.33))
+'''
+
+
+'''
+complex（复数）
+复数有整数和虚 数组成，可以用a+bj或者complex（a+b）表示，复数的实部和虚部都是浮点型
+'''
+'''
+print(complex(5,6))
+'''
+
